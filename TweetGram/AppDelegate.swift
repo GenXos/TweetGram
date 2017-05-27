bd16356604d7005f2d94c1cc45dd77d21bb6b68e
Todd Fields <toddwfields@gmail.com>
2017-05-27 08:57:15 -0500

    Initial Commit

---
+++ TweetGram/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TweetGram
+//
+//  Created by Todd Fields on 2017-05-27.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TweetGram/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TweetGram
+//
+//  Created by Todd Fields on 2017-05-27.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
